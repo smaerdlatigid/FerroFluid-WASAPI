44469e2515ac8ec399f3c20f548640c5408d0ff1
pearsonkyle <pearsonk@email.arizona.edu>
2020-03-04 23:03:37 -0700

    freshies

---
+++ Assets/Samples/ferro/Shader2VFX.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class Shader2VFX : MonoBehaviour
+{
+    public LoopbackAudio Audio;
+    VisualEffect vis;
+
+    void Start()
+    {
+        vis =  GetComponent<VisualEffect>();
+    }
+
+    Color height; 
+    public float normalizedVolume = 0.1f;
+
+    void Update()
+    {
+        vis.SetFloat("AudioAverage", Audio.WeightedAverage);
+        vis.SetFloat("AudioSub",Audio.WeightedPostScaledSpectrumData[0] );
+               
+        float[,] audioData = Audio.getBuffer(0);
+
+        Texture2D texture = new Texture2D(Audio.bufferSize, Audio.SpectrumSize);
+
+        for (int y = 0; y < Audio.SpectrumSize; y++)
+        {
+            for (int x = 0; x < Audio.bufferSize; x++)
+            {
+                height = new Color(
+                    audioData[Audio.bufferSize-x-1, y]*normalizedVolume,
+                    audioData[Audio.bufferSize-x-1, y]*normalizedVolume,
+                    audioData[Audio.bufferSize-x-1, y]*normalizedVolume,
+                    1f
+                );
+                texture.SetPixel(x, y, height);
+            }
+        }
+        texture.Apply();
+        vis.SetTexture("AudioSpectrum", texture);
+    }
+}
